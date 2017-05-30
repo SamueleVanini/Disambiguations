e493ddca0eb8ddba0e443d6467faee8e3dbf710c
SamueleVanini <samuele.vanini@live.it>
2017-05-30 11:10:24 +0200

    Creazione package per moduli pyhon e aggiunta directory con file .ttl su cui si crea il dizionario

---
+++ Components/Lettura_file.py
@@ -0,0 +1,30 @@
+import codecs
+
+dict = {}
+
+def create_dict(file, word_to_search):
+    list_help = []
+    i = 0
+    dict_stop = False
+    for raw in file:
+        print (i)
+        if raw != '# started 2015-01-29T09:18:58Z\r\n' or raw != '# completed 2015-01-29T09:54:37Z\r\n':
+            key = raw.split()[0]
+            value = raw.split()[2]
+            if key.upper() == '<HTTP://IT.DBPEDIA.ORG/RESOURCE/'+word_to_search.upper()+'>':
+                dict_stop = True
+                list_help.append(value)
+                dict[key] = tuple(list_help)
+            if dict_stop == True and key.upper() > '<HTTP://IT.DBPEDIA.ORG/RESOURCE/'+word_to_search.upper()+'>':
+                break
+        i += 1
+
+def main():
+    file = codecs.open('File_dbpedia/itwiki-20150121-disambiguations.ttl', 'rU', 'utf-8')
+    #file = codecs.open('prova', 'rU', 'utf-8')
+    word_to_search = 'Basic'
+    create_dict(file, word_to_search)
+    print (dict)
+
+if __name__ == '__main__':
+    main()
---
+++ Components/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['search_in_page', 'Lettura_file']
+
+from Components import search_in_page, Lettura_file
